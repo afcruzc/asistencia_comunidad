a5484b10f687d1b337cbdf1943038c2db0a290d3
Andres Cruz <afcruzc@gmail.com>
2025-09-16 16:49:14 -0500

    ajuste de consumo a la API como /api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+const API_URL = process.env.REACT_APP_API_URL || '/api';
 
 // Función para obtener el token del localStorage
 const getToken = () => localStorage.getItem('leaderToken');
